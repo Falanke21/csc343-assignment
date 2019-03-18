1dd6b40193aabdaa2d3ef6628c0aeb62c4e82150
Falanke21 <1065314944@qq.com>
2019-03-18 16:19:39 -0400

    fixed bug in q3

--- q3.sql
+++ q3.sql
@@ -22,11 +22,13 @@ DROP VIEW IF EXISTS winningParty CASCADE;
 DROP VIEW IF EXISTS partyToCountWins CASCADE;
 DROP VIEW IF EXISTS averages CASCADE;
 DROP VIEW IF EXISTS resultParties1 CASCADE;
+DROP VIEW IF EXISTS winnersAndElections CASCADE;
+DROP VIEW IF EXISTS winnersAndMostRecentYear CASCADE;
+DROP VIEW IF EXISTS winnersAndElectionsInfo CASCADE;
 DROP VIEW IF EXISTS resultParties2 CASCADE;
 DROP VIEW IF EXISTS resultParties3 CASCADE;
 DROP VIEW IF EXISTS resultParties4 CASCADE;
 DROP VIEW IF EXISTS resultParties5 CASCADE;
-DROP VIEW IF EXISTS resultParties6 CASCADE;
 
 -- find the winning party of each election
 CREATE VIEW winningParty AS
@@ -64,59 +66,77 @@ FROM averages,
 WHERE averages.country_id = temp.country_id
 AND temp.count - averages.avg > 3;
 
--- add party family
-CREATE VIEW resultParties2 AS
-SELECT resultParties1.country_id, resultParties1.winners, party_family.family
-FROM resultParties1
-LEFT JOIN party_family ON party_family.party_id = resultParties1.winners;
+-- FIXING
 
--- add won elections number
+-- winners and all its associated elections
+CREATE VIEW winnersAndElections AS
+SELECT winners, election_id
+FROM resultParties1 JOIN winningParty
+ON resultParties1.winners = winningParty.party_id;
+
+-- winners and its most recent time of won election
+CREATE VIEW winnersAndMostRecentYear AS
+SELECT winners, MAX(election.e_date) AS most_recent_time
+FROM winnersAndElections JOIN election
+ON winnersAndElections.election_id = election.id
+GROUP BY winners;
+
+-- winners and its most recent TIME of won election, and that election_id
+CREATE VIEW winnersAndElectionsInfo AS
+SELECT winnersAndMostRecentYear.winners, election.id AS election_id,
+winnersAndMostRecentYear.most_recent_time
+FROM winnersAndMostRecentYear JOIN election
+ON winnersAndMostRecentYear.most_recent_time = election.e_date;
+
+-- winners and its most recent YEAR of won election, and that election_id
+CREATE VIEW resultParties2 AS
+SELECT resultParties1.country_id, resultParties1.winners,
+winnersAndElectionsInfo.election_id,
+EXTRACT(YEAR from winnersAndElectionsInfo.most_recent_time) AS year
+FROM winnersAndElectionsInfo JOIN resultParties1
+ON winnersAndElectionsInfo.winners = resultParties1.winners;
+
+-- add info about how many elections won as wonElections
 CREATE VIEW resultParties3 AS
-SELECT resultParties2.country_id, resultParties2.winners, resultParties2.family,
-partyToCountWins.count AS win_elec_num
+SELECT country_id, winners, election_id, year,
+partyToCountWins.count AS wonElections
 FROM resultParties2 JOIN partyToCountWins
 ON resultParties2.winners = partyToCountWins.party_id;
 
--- add mostRecentlyWonElectionYear
+-- add party family info, notice left join since family could be null
 CREATE VIEW resultParties4 AS
-SELECT resultParties3.country_id, resultParties3.winners, resultParties3.family,
-resultParties3.win_elec_num, max(EXTRACT(YEAR from election.e_date)) as most_recent_year
-FROM resultParties3, winningParty, election
-WHERE resultParties3.winners = winningParty.party_id
-AND election.id = winningParty.election_id
-GROUP BY resultParties3.country_id, resultParties3.family,
-resultParties3.win_elec_num, election.id, resultParties3.winners;
+SELECT country_id, winners, party_family.family, wonElections, election_id, year
+FROM resultParties3 LEFT JOIN party_family
+ON winners = party_id;
 
--- add mostRecentlyWonElectionId
+-- replace party_id and country_id as party name and country name
 CREATE VIEW resultParties5 AS
-SELECT resultParties4.country_id, resultParties4.winners, resultParties4.family,
-resultParties4.win_elec_num, election.id AS election_id, resultParties4.most_recent_year
-FROM resultParties4, election
-WHERE resultParties4.most_recent_year = EXTRACT(YEAR from election.e_date)
-AND resultParties4.country_id = election.country_id;
-
--- add country name and party name
-CREATE VIEW resultParties6 AS
 SELECT country.name AS countryName, party.name AS partyName,
-resultParties5.family AS partyFamily, resultParties5.win_elec_num AS wonElections,
-resultParties5.election_id AS mostRecentlyWonElectionId,
-resultParties5.most_recent_year AS mostRecentlyWonElectionYear
-FROM resultParties5, country, party
-WHERE country.id = resultParties5.country_id
-AND party.id = resultParties5.winners;
--- ORDER BY countryName, wonElections, partyName DESC
+family AS partyFamily, wonElections,
+election_id AS mostRecentlyWonElectionId,
+year AS mostRecentlyWonElectionYear
+FROM resultParties4, party, country
+WHERE resultParties4.winners = party.id
+AND resultParties4.country_id = country.id;
+
+
+-- FIXING
+
+-- -- ORDER BY countryName, wonElections, partyName DESC
 
 
 insert into q3 (countryName, partyName, partyFamily, wonElections, mostRecentlyWonElectionId, mostRecentlyWonElectionYear)
 SELECT *
-FROM resultParties6;
+FROM resultParties5;
 
 DROP VIEW IF EXISTS winningParty CASCADE;
 DROP VIEW IF EXISTS partyToCountWins CASCADE;
 DROP VIEW IF EXISTS averages CASCADE;
 DROP VIEW IF EXISTS resultParties1 CASCADE;
+DROP VIEW IF EXISTS winnersAndElections CASCADE;
+DROP VIEW IF EXISTS winnersAndMostRecentYear CASCADE;
+DROP VIEW IF EXISTS winnersAndElectionsInfo CASCADE;
 DROP VIEW IF EXISTS resultParties2 CASCADE;
 DROP VIEW IF EXISTS resultParties3 CASCADE;
 DROP VIEW IF EXISTS resultParties4 CASCADE;
 DROP VIEW IF EXISTS resultParties5 CASCADE;
-DROP VIEW IF EXISTS resultParties6 CASCADE;
